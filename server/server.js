b995ad82cb80290fc0e6a49749b071e79eaf82c0
Jackson <z5397791@ad.unsw.edu.au>
2023-01-30 22:09:51 +1100

    Add cors middleware to server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// OpenAi instance
 const openai = new OpenAIApi(configuration);
 
+// Express initialize
 const app = express();
+
+// Middlewares
+app.use(cors());
